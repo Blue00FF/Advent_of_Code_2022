c9e8674ac030c1b7e5da67791cb1b7ac4e0d9edc
Fabrizio Martone <53048926+Blue00FF@users.noreply.github.com>
2022-12-18 04:45:17 +0000

    Rewrote the rope class
    The Rope class has been rewritten to generalise to a rope with n nodes
    Problem: not all the nodes move when they're supposed to, they do so only up to a certain point (5th node or so)

--- Day 9 - Rope Bridge/main.py
+++ Day 9 - Rope Bridge/main.py
@@ -1,9 +1,8 @@
 class Rope:
-    def __init__(self) -> None:
-        self.head_x = 0
-        self.head_y = 0
-        self.tail_x = 0
-        self.tail_y = 0
+    def __init__(self, rope_length) -> None:
+        self.ROPE_LENGTH = rope_length
+        self.nodes_x = [0] * rope_length
+        self.nodes_y = [0] * rope_length
         self.movement_counter = 0
         self.movement_direction = None
         self.commands = []
@@ -12,33 +11,31 @@ class Rope:
     def set_commands(self, content):
         self.commands = content.split("\n")
 
-    def leave_trail(self, grid):
-        grid[self.tail_x][self.tail_y].is_visited = True
-
     def move_head(self):
         match self.movement_direction:
             case "U":
-                self.head_x -= 1
+                self.nodes_x[0] -= 1
             case "D":
-                self.head_x += 1
+                self.nodes_x[0] += 1
             case "L":
-                self.head_y -= 1
+                self.nodes_y[0] -= 1
             case "R":
-                self.head_y += 1
+                self.nodes_y[0] += 1
         self.movement_counter -= 1
 
     def move_tail(self):
-        delta_x = self.head_x - self.tail_x
-        delta_y = self.head_y - self.tail_y
-        if delta_x == 0 and abs(delta_y) > 1:
-            self.tail_y += delta_y // abs(delta_y)
-        elif delta_y == 0 and abs(delta_x) > 1:
-            self.tail_x += delta_x // abs(delta_x)
-        elif (abs(delta_x) > 1 and abs(delta_y) == 1) or (
-            abs(delta_y) > 1 and abs(delta_x) == 1
-        ):
-            self.tail_x += delta_x // abs(delta_x)
-            self.tail_y += delta_y // abs(delta_y)
+        for index in range(1, self.ROPE_LENGTH):
+            delta_x = self.nodes_x[index - 1] - self.nodes_x[index]
+            delta_y = self.nodes_y[index - 1] - self.nodes_y[index]
+            if delta_x == 0 and abs(delta_y) > 1:
+                self.nodes_y[index] += delta_y // abs(delta_y)
+            elif delta_y == 0 and abs(delta_x) > 1:
+                self.nodes_x[index] += delta_x // abs(delta_x)
+            elif (abs(delta_x) > 1 and abs(delta_y) == 1) or (
+                abs(delta_y) > 1 and abs(delta_x) == 1
+            ):
+                self.nodes_x[index] += delta_x // abs(delta_x)
+                self.nodes_y[index] += delta_y // abs(delta_y)
 
     def no_movement_left(self):
         return self.movement_counter == 0
@@ -49,23 +46,34 @@ class Rope:
         self.commands.pop(0)
 
     def start_movement(self):
+        self.history = []
         while True:
-            self.history.append((self.tail_x, self.tail_y))
+            self.history.append((self.nodes_x[-1], self.nodes_y[-1]))
             if self.no_movement_left():
                 if len(self.commands) == 0:
                     break
                 self.get_next_command()
             self.move_head()
             self.move_tail()
+            # self.visualise_rope()
 
     def calculate_total_visited(self):
         return len(set(self.history))
+
+    def visualise_rope(self):
+        print()
+        for i in zip(self.nodes_x, self.nodes_y):
+            print(i)
 
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         content = f.read()
-    rope = Rope()
-    rope.set_commands(content)
-    rope.start_movement()
-    print(rope.calculate_total_visited())
+    rope_1 = Rope(2)
+    rope_1.set_commands(content)
+    rope_1.start_movement()
+    print(rope_1.calculate_total_visited())
+    rope_2 = Rope(10)
+    rope_2.set_commands(content)
+    rope_2.start_movement()
+    print(rope_2.calculate_total_visited())
--- Day 9 - Rope Bridge/test.py
+++ Day 9 - Rope Bridge/test.py
@@ -1,6 +1,6 @@
 from main import *
 
-test_input = """R 4
+test_input_1 = """R 4
 U 4
 L 3
 D 1
@@ -9,10 +9,38 @@ D 1
 L 5
 R 2"""
 
-rope = Rope()
-rope.set_commands(test_input)
-rope.start_movement()
+test_input_2 = """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"""
 
 
-def test_calculate_total_visited():
+def test_calculate_total_visited_length_2_input_1():
+    rope = Rope(2)
+    rope.set_commands(test_input_1)
+    rope.start_movement()
     assert rope.calculate_total_visited() == 13
+
+
+def test_calculate_total_visited_length_10_input_1():
+    rope = Rope(10)
+    rope.set_commands(test_input_1)
+    rope.start_movement()
+    assert rope.calculate_total_visited() == 1
+
+
+def test_calculate_total_visited_length_10_input_2():
+    rope = Rope(10)
+    rope.set_commands(test_input_2)
+    rope.start_movement()
+    assert rope.calculate_total_visited() == 36
+
+
+if __name__ == "__main__":
+    rope = Rope(10)
+    rope.set_commands(test_input_2)
+    rope.start_movement()
