7cf927212ddb2511bd002b9365ca439cf425abca
Fabrizio Martone <53048926+Blue00FF@users.noreply.github.com>
2022-12-22 10:37:26 +0000

    test.py refactor
    Refactored the testing script to be usable by pytest

--- Day 12 - Hill Climbing Algorithm/test.py
+++ Day 12 - Hill Climbing Algorithm/test.py
@@ -6,6 +6,23 @@ accszExk
 acctuvwj
 abdefghi"""
 
-Square.convert_to_grid(test_input)
-Square.determine_all_possible_moves()
-print(Square.find_path_to_finish())
+
+def test_find_path_from_start_to_finish():
+    Square.convert_to_grid(test_input)
+    Square.determine_all_possible_moves()
+    current_record = Square.find_path_to_finish()
+    assert current_record == 31
+
+
+def test_find_scenic_path():
+    Square.convert_to_grid(test_input)
+    Square.determine_all_possible_moves()
+    current_record = Square.find_path_to_finish()
+    previous_start = (0, 0)
+    for possible_start in Square.scenic_starts:
+        Square.reset_grid(previous_start, possible_start)
+        new_path_length = Square.find_path_to_finish()
+        if new_path_length < current_record:
+            current_record = new_path_length
+        previous_start = possible_start
+    assert current_record == 29
