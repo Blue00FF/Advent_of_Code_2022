27523bfe3cf9e32ee18738313705f3801add99fa
Fabrizio Martone <53048926+Blue00FF@users.noreply.github.com>
2022-12-23 11:05:44 +0000

    Day 14 Submission

--- Day 14 - Regolith Reservoir/main.py
+++ Day 14 - Regolith Reservoir/main.py
@@ -1,14 +1,24 @@
 class Block:
     grid = []
-    GRID_HEIGHT = 600
-    GRID_LENGTH = 600
+    GRID_HEIGHT = 1000
+    GRID_LENGTH = 1000
     active_block = (0, 500)
     sand_counter = 0
     is_finished = False
+    floor_height = 0
 
-    def start_sand():
+    def start_sand_without_floor():
+        Block.sand_counter = 0
         while not Block.is_finished:
-            Block.grid[Block.active_block[0]][Block.active_block[1]].fall()
+            Block.grid[Block.active_block[0]][Block.active_block[1]].endless_fall()
+
+    def start_sand_with_floor():
+        Block.sand_counter = 0
+        for y in range(Block.GRID_LENGTH):
+            Block.grid[Block.floor_height + 2][y].turn_into_rock()
+        Block.is_finished = False
+        while not Block.is_finished:
+            Block.grid[Block.active_block[0]][Block.active_block[1]].finite_fall()
 
     def process_input(content):
         Block.generate_grid()
@@ -27,27 +37,42 @@ class Block:
                         previous_coordinates,
                         current_coordinates,
                     )
+                if current_coordinates[1] > Block.floor_height:
+                    Block.floor_height = current_coordinates[1]
                 for x in range(previous_coordinates[1], current_coordinates[1] + 1):
                     Block.grid[x][previous_coordinates[0]].turn_into_rock()
                 for y in range(previous_coordinates[0], current_coordinates[0] + 1):
                     Block.grid[previous_coordinates[1]][y].turn_into_rock()
 
     def generate_grid():
+        Block.grid = []
         for x in range(Block.GRID_HEIGHT):
             Block.grid.append([])
             for y in range(Block.GRID_LENGTH):
                 Block.grid[x].append(Block(x, y, "air"))
+
+    def visualise_grid():
+        for x in range(12):
+            for y in range(488, 513):
+                if Block.grid[x][y].is_air():
+                    print(".", sep="", end="")
+                elif Block.grid[x][y].block_type == "sand":
+                    print("+", sep="", end="")
+                else:
+                    print("#", sep="", end="")
+            print()
 
     def __init__(self, x_pos, y_pos, block_type):
         self.block_type = block_type
         self.x_pos = x_pos
         self.y_pos = y_pos
 
-    def fall(self):
+    def endless_fall(self):
         down = Block.grid[self.x_pos + 1][self.y_pos]
         down_left = Block.grid[self.x_pos + 1][self.y_pos - 1]
         down_right = Block.grid[self.x_pos + 1][self.y_pos + 1]
         if self.x_pos >= Block.GRID_HEIGHT - 2:
+            Block.active_block = (0, 500)
             Block.is_finished = True
         elif down.is_air():
             down.turn_into_sand()
@@ -63,6 +88,29 @@ class Block:
             Block.active_block = (down_right.x_pos, down_right.y_pos)
         else:
             Block.sand_counter += 1
+            Block.active_block = (0, 500)
+            Block.grid[0][500].turn_into_sand()
+
+    def finite_fall(self):
+        down = Block.grid[self.x_pos + 1][self.y_pos]
+        down_left = Block.grid[self.x_pos + 1][self.y_pos - 1]
+        down_right = Block.grid[self.x_pos + 1][self.y_pos + 1]
+        if down.is_air():
+            down.turn_into_sand()
+            self.turn_into_air()
+            Block.active_block = (down.x_pos, down.y_pos)
+        elif down_left.is_air():
+            down_left.turn_into_sand()
+            self.turn_into_air()
+            Block.active_block = (down_left.x_pos, down_left.y_pos)
+        elif down_right.is_air():
+            down_right.turn_into_sand()
+            self.turn_into_air()
+            Block.active_block = (down_right.x_pos, down_right.y_pos)
+        else:
+            Block.sand_counter += 1
+            if Block.active_block == (0, 500):
+                Block.is_finished = True
             Block.active_block = (0, 500)
             Block.grid[0][500].turn_into_sand()
 
@@ -86,5 +134,8 @@ if __name__ == "__main__":
     with open("input.txt") as f:
         content = f.read()
     Block.process_input(content)
-    Block.start_sand()
+    Block.start_sand_without_floor()
+    print(Block.sand_counter)
+    Block.process_input(content)
+    Block.start_sand_with_floor()
     print(Block.sand_counter)
--- Day 14 - Regolith Reservoir/test.py
+++ Day 14 - Regolith Reservoir/test.py
@@ -3,8 +3,17 @@ from main import *
 test_input = """498,4 -> 498,6 -> 496,6
 503,4 -> 502,4 -> 502,9 -> 494,9"""
 
+Block.GRID_HEIGHT = 20
+Block.GRID_LENGTH = 600
 
-def test_sand_counter():
+
+def test_sand_counter_without_floor():
     Block.process_input(test_input)
-    Block.start_sand()
+    Block.start_sand_without_floor()
     assert Block.sand_counter == 24
+
+
+def test_sand_counter_with_floor():
+    Block.process_input(test_input)
+    Block.start_sand_with_floor()
+    assert Block.sand_counter == 93
